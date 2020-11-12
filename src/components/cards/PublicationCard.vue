06d835fe957e446965f67af7eb4d63fd08723f60
Hovhannes Nazaryan <hovnaz@tenflares.com>
2020-11-12 18:29:20 +0400

    insight mobile version

--- src/components/cards/PublicationCard.vue
+++ src/components/cards/PublicationCard.vue
@@ -6,7 +6,7 @@
     </div>
     <div class="info--box">
       <h6 class="created--date caption-m--regular mb-16">{{ item.created_at }}</h6>
-      <h3 class="title body-m--medium line-height-32 mb-16">{{ item.title }}</h3>
+      <h3 class="title body-mobile-l--medium body-m--medium line-height-32 mb-16">{{ item.title }}</h3>
       <ul class="d-flex align-center author--box">
         <li class="w-24 h-24"><img :src="item.author.image" :alt="item.author.name" /></li>
         <li class="caption-m--regular ml-8 text-black">{{ item.author.name }}</li>
--- src/views/Insights.vue
+++ src/views/Insights.vue
@@ -2,11 +2,11 @@
   <section id="welcome-insights">
     <section class="container">
       <section class="items">
-        <div>
+        <div class="mt-mobile-16">
           <Badge text="publications"/>
-          <h1 class="heading-l--light line-height-64 mt-31">Best Tasting Plant-Based Complete Nutrition on the Market</h1>
+          <h1 class="heading-mobile-xs--light heading-l--light line-height-64 mt-mobile-16 mt-31 line-height-mobile-36">Best Tasting Plant-Based Complete Nutrition on the Market</h1>
 
-          <ul class="d-flex align-center navigation mt-24">
+          <ul class="d-flex align-center navigation mt-24 mt-mobile-16">
             <li>
               <ul class="d-flex align-center">
                 <li class="w-24 h-24"><img src="/img/avatar_2.png" alt="Pedro Stauu"/></li>
@@ -19,7 +19,7 @@
             </li>
           </ul>
 
-          <Button text="Read more" icon="arrow-right" class="mt-40" size="lg"/>
+          <Button text="Read more" icon="arrow-right" class="mt-40 mt-mobile-32" size="lg" mobile="md"/>
         </div>
         <div>
           <div class="img-abs">
@@ -33,10 +33,16 @@
   <section id="insight-featured--section">
     <section class="container">
       <p class="overline-s--medium text-uppercase mb-16 text-stone-gray mb-mobile-16 letter-spacing-08em">insights</p>
-      <h2 class="heading-m--regular mb-64">Featured publications</h2>
+      <h2 class="heading-mobile-s--regular heading-m--regular mb-mobile-40 mb-64">Featured publications</h2>
 
-      <section class="featured--items">
+      <section class="featured--items d-mobile-none d-desktop-block">
         <PublicationCard v-for="publication in publications" :item="publication"/>
+      </section>
+
+      <section class="articles-box d-mobile-block d-desktop-none">
+        <section class="article-cards">
+          <ArticleCard v-for="publication in publications" :item="publication" />
+        </section>
       </section>
     </section>
   </section>
@@ -46,21 +52,27 @@
       <section class="items">
         <div class="item">
           <p class="overline-s--medium text-uppercase mb-16 text-stone-gray mb-mobile-16 letter-spacing-08em">insights</p>
-          <h2 class="heading-m--regular mb-64">Categories</h2>
+          <h2 class="heading-mobile-s--regular heading-m--regular mb-mobile-40 mb-64">Categories</h2>
         </div>
 
         <div class="item">
           <div>
-            <h2 class="heading-xs--medium mb-32">Shows</h2>
-            <ul>
+            <h2 class="body-mobile-l--medium heading-xs--medium mb-mobile-24 mb-32 d-flex align-center"
+                @click="openTab('shows')"
+                :class="{'opened' : category === 'shows'}"
+            >
+              Shows
+              <Icon href="chevron-down" fill="#000" class="ml-14 mt-2 d-block-mobile d-desktop-none" size="12"/>
+            </h2>
+            <ul :class="{'opened' : category === 'shows'}" class="mb-mobile-24">
               <li>
-                <router-link to="#" class="body-m--regular mb-32">Case studies</router-link>
+                <router-link to="#" class="body-m--regular mb-mobile-12 mb-32">Case studies</router-link>
               </li>
               <li>
-                <router-link to="#" class="body-m--regular mb-32">Shows</router-link>
+                <router-link to="#" class="body-m--regular mb-mobile-12 mb-32">Shows</router-link>
               </li>
               <li>
-                <router-link to="#" class="body-m--regular mb-32">
+                <router-link to="#" class="body-m--regular">
                   Blog
                   <Icon href="external-link" size="12" class="ml-8"/>
                 </router-link>
@@ -69,34 +81,46 @@
           </div>
 
           <div>
-            <h2 class="heading-xs--medium mb-32">Publications</h2>
-            <ul>
+            <h2 class="body-mobile-l--medium heading-xs--medium mb-mobile-24 mb-32 d-flex align-center"
+                @click="openTab('publications')"
+                :class="{'opened' : category === 'publications'}"
+            >
+              Publications
+              <Icon href="chevron-down" fill="#000" class="ml-14 mt-2 d-block-mobile d-desktop-none" size="12"/>
+            </h2>
+            <ul :class="{'opened' : category === 'publications'}" class="mb-mobile-24">
               <li>
-                <router-link to="#" class="body-m--regular mb-32">Managment</router-link>
+                <router-link to="#" class="body-m--regular mb-mobile-12 mb-32">Managment</router-link>
               </li>
               <li>
-                <router-link to="#" class="body-m--regular mb-32">Product</router-link>
+                <router-link to="#" class="body-m--regular mb-mobile-12 mb-32">Product</router-link>
               </li>
               <li>
-                <router-link to="#" class="body-m--regular mb-32">Technology</router-link>
+                <router-link to="#" class="body-m--regular mb-mobile-12 mb-32">Technology</router-link>
               </li>
               <li>
-                <router-link to="#" class="body-m--regular mb-32">Culture</router-link>
+                <router-link to="#" class="body-m--regular">Culture</router-link>
               </li>
             </ul>
           </div>
 
           <div>
-            <h2 class="heading-xs--medium mb-32">Articles</h2>
-            <ul>
+            <h2 class="body-mobile-l--medium heading-xs--medium d-flex align-center mb-32 mb-mobile-0"
+                @click="openTab('articles')"
+                :class="{'opened' : category === 'articles'}"
+            >
+              Articles
+              <Icon href="chevron-down" fill="#000" class="ml-14 mt-2 d-block-mobile d-desktop-none" size="12"/>
+            </h2>
+            <ul :class="{'opened' : category === 'articles'}" class="mt-mobile-16">
               <li>
-                <router-link to="#" class="body-m--regular mb-32">Featured</router-link>
+                <router-link to="#" class="body-m--regular mb-mobile-12 mb-32">Featured</router-link>
               </li>
               <li>
-                <router-link to="#" class="body-m--regular mb-32">Most popular</router-link>
+                <router-link to="#" class="body-m--regular mb-mobile-12 mb-32">Most popular</router-link>
               </li>
               <li>
-                <router-link to="#" class="body-m--regular mb-32">Recent</router-link>
+                <router-link to="#" class="body-m--regular">Recent</router-link>
               </li>
             </ul>
           </div>
@@ -108,14 +132,14 @@
   <section class="insight-articles-section">
     <section class="container">
       <p class="overline-s--medium text-uppercase mb-16 text-stone-gray mb-mobile-16 letter-spacing-08em">insights</p>
-      <h2 class="heading-m--regular mb-64">Case studies</h2>
+      <h2 class="heading-mobile-s--regular heading-m--regular mb-mobile-40 mb-64">Case studies</h2>
 
       <section class="articles-box">
         <section class="article-cards">
           <ArticleCard v-for="study in studies" :item="study" />
         </section>
 
-        <section class="d-flex justify-content-center mt-80">
+        <section class="d-flex justify-content-center mt-mobile-20 mt-80">
           <Button text="Read all publications" icon="arrow-right" size="lg" type="secondary"/>
         </section>
       </section>
@@ -125,14 +149,14 @@
   <section class="insight-articles-section bg-light-gray">
     <section class="container">
       <p class="overline-s--medium text-uppercase mb-16 text-stone-gray mb-mobile-16 letter-spacing-08em">insights</p>
-      <h2 class="heading-m--regular mb-64">Shows</h2>
+      <h2 class="heading-mobile-s--regular heading-m--regular mb-mobile-40 mb-64">Shows</h2>
 
       <section class="articles-box">
         <section class="article-cards">
           <ArticleCard v-for="show in shows" :item="show" />
         </section>
 
-        <section class="d-flex justify-content-center mt-80">
+        <section class="d-flex justify-content-center mt-mobile-20 mt-80">
           <Button text="Check more shows" icon="arrow-right" size="lg" type="secondary"/>
         </section>
       </section>
@@ -142,14 +166,14 @@
   <section class="insight-articles-section">
     <section class="container">
       <p class="overline-s--medium text-uppercase mb-16 text-stone-gray mb-mobile-16 letter-spacing-08em">insights</p>
-      <h2 class="heading-m--regular mb-64">Trending articles</h2>
+      <h2 class="heading-mobile-s--regular heading-m--regular mb-mobile-40 mb-64">Trending articles</h2>
 
       <section class="articles-box">
         <section class="article-cards">
           <ArticleCard v-for="study in studies" :item="study" />
         </section>
 
-        <section class="d-flex justify-content-center mt-80">
+        <section class="d-flex justify-content-center mt-mobile-20 mt-80">
           <Button text="Read all publications" icon="arrow-right" size="lg" type="secondary"/>
         </section>
       </section>
@@ -264,6 +288,13 @@ export default {
           }
         },
       ],
+      category : null
+    }
+  },
+  methods : {
+    openTab(category) {
+      if(category === this.category) this.category = null;
+      else this.category = category;
     }
   }
 }
